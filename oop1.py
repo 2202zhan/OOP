d28d2ac468e98d6fb8a622df36ff61ce4cc17f8e
2202zhan <zklzanov@gmail.com>
2024-09-16 11:35:28 +0500

    Добавлен файл oop1.py

---
+++ oop1.py
@@ -0,0 +1,19 @@
+import math
+
+
+class Taskffg:
+    def __init__(self, n): 
+        self.n = n
+
+    def calc(self):
+        res = 0
+        sum_val = 0  
+        for i in range(1, self.n + 1):  
+            sum_val = sum_val + math.sin(i)  
+            res = res + 1 / sum_val  
+        return res
+
+n = int(input("Введите n: "))  
+taskffg = Taskffg(n)
+res = taskffg.calc()
+print(res)
